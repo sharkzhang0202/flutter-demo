e4a6830050b89062f076de15554faf11401ea373
kingzhang1228 <zhang_xxyy66@163.com>
2019-04-30 15:25:07 +0800

    新建详情页说明功功能

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import '../provide/details_info.dart';
 import './details_page/details_top_area.dart';
+import './details_page/details_explain.dart';
 class DetailsPage extends StatelessWidget {
   final String goodsId;
   DetailsPage(this.goodsId);
@@ -25,7 +26,8 @@ class DetailsPage extends StatelessWidget {
             return Container(
               child: Column(
                 children: <Widget>[
-                 DetailsTopArea()
+                 DetailsTopArea(),
+                 DetailsExplain()
                 ],
               )
             );
